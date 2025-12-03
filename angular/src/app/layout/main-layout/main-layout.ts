5ddaeee9afd8fb0ae982bd4495e0543b76aceae9
vishnue.bn <vishnue.bn@pitsolutions.com>
2025-12-03 12:22:26 +0530

    fix: Logout functionality added

--- angular/src/app/layout/main-layout/main-layout.ts
+++ angular/src/app/layout/main-layout/main-layout.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLinkActive, RouterLink, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet,RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet,RouterLink, RouterLinkActive],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
 export class MainLayout {
 menuOpen = false;
 
+constructor(private authService: AuthService, private router: Router) {}
+
 toggleMenu() {
   this.menuOpen = !this.menuOpen;
+}
+
+get isAuthenticated(): boolean {
+  return this.authService.isAuthenticated();
+}
+
+logout() {
+  this.authService.removeToken();
+  this.router.navigate(['/login']);
 }
 }
